564204cd26862bcb1b4cc0976c773fec60d835b8
Peter Bowman <bowmanpete123@gmail.com>
2018-06-30 21:01:42 +1200

    not a virgin anymore

--- src/website_script.js
+++ src/website_script.js
@@ -10,7 +10,7 @@ function compSelect(min, max){
 var shipPlace = compSelect(1,7);
 // Computer Places ship within the sea
 for(var i = 0; i < grid.length; i++){
-  if(i == compSelect){ //dick
+  if(i == compSelect){
     grid[i] = 1;
   }
   else{
