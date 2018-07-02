643d085e405c52e1fbfa69cd8795dc333a1f6a3a
mesa123123 <bowmanpete123@gmail.com>
2018-07-02 16:15:03 +1200

    Re-upload to make sure changes from last night went through

--- src/main/website_script.js
+++ src/main/website_script.js
@@ -28,33 +28,33 @@ function fireRound(){
   var selection = document.getElementById("fireConsole").value;
   selection--;
   if( shipAlive && playerAlive && (isNaN(selection) || grid[selection] == -1 || selection < 0 || selection > 6) ){
-    document.getElementById("intelConsole").innerHTML = "Intel: Invalid Selection, Make another one";
+    document.getElementById("intelConsole").innerHTML = "Invalid Selection, Make another one";
   }
   else if ( playerAlive && shipAlive){
     i--;
     //Check the selection is valid and the User isn't bombing the same place
     //Case: Missed shot else: hit
     if(grid[selection] == 0){
-      document.getElementById("Result").innerHTML = "Intel: It appears you missed, enemy ship still active";
+      document.getElementById("Result").innerHTML = "It appears you missed, enemy ship still active";
       grid[selection] = -1;
     } else{
-      document.getElementById("Result").innerHTML = "Intel: Direct hit! You've got the bastard!";
+      document.getElementById("Result").innerHTML = "Direct hit! You've got the bastard!";
       shipAlive = false;
     }
     if(i > 0){
       //Messages that inform user of ship status; this creates dramatic effect
       switch(i){
         case 3:
-            document.getElementById("intelConsole").innerHTML = "Intel: Attained visuals on ship;  Where should we fire?";
+            document.getElementById("intelConsole").innerHTML = "Radar has picked up an enemy ship;  Where should we fire?";
           break;
         case 2:
-            document.getElementById("intelConsole").innerHTML = "Intel: Enemy Ship Loading Ammunition; Where should we fire?";
+            document.getElementById("intelConsole").innerHTML = "Enemy Ship Loading Ammunition; Where should we fire?";
           break;
         case 1:
-            document.getElementById("intelConsole").innerHTML = "Intel: Enemy Ship Taking Aim; Where should we fire?";
+            document.getElementById("intelConsole").innerHTML = " Enemy Ship Taking Aim; Where should we fire?";
           break;
         default:
-            document.getElementById("intelConsole").innerHTML = "Intel: Attained visuals on ship;  Where should we fire?";
+            document.getElementById("intelConsole").innerHTML = " Attained visuals on ship;  Where should we fire?";
         //Check the selection is valid and the User isn't bombing the same place
         //Case: Missed shot
       } //end of switch
@@ -70,13 +70,13 @@ function result(){
   //console summarizes the results of the Game
   //Lose
   if( shipAlive== true ){
-    document.getElementById("intelConsole").innerHTML = ("Intel: It appears as though we gave them enough time to fire... \
+    document.getElementById("intelConsole").innerHTML = (" It appears as though we gave them enough time to fire... \
     I believe, as it turns out, that you are quite dead... Sorry :/");
     playerAlive = false;
   }
   //Win
   else{
-    document.getElementById("intelConsole").innerHTML = ("Intel: Bloody well done there! Were safe for another day!");
+    document.getElementById("intelConsole").innerHTML = (" Bloody well done there! Were safe for another day!");
   }
 }
 
