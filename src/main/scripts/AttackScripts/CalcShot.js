2a74e1f89b86559337e1665148f7dbf9dc00da7b
mesa123123 <bowmanpete123@gmail.com>
2018-07-11 20:23:59 +1200

    Completed Logic for firing and started the beginning of a controller that will interact with the page

--- src/main/scripts/AttackScripts/CalcShot.js
+++ src/main/scripts/AttackScripts/CalcShot.js
@@ -31,23 +31,30 @@ function createTargets(grid, userSelection){
 A function that will do the math of calculating hits misses etc.
 */
 
-function calcShot(shotArray, userSelection, Grid) {
+function calcShot(shotArray, userSelection, Grid, scoreCard) {
       var shotArray = createTargets(userSelection, Grid);
       /*This is an array that will stack up the hits and misses
        and repeats*/
-      var tally = [0, 0, 0];
       for(var i = 0; i < shotArray.length; i++){
         for(var j = 0; j < shotArray[i].length){
-          if(Grid.grid[i][j] == 0){
-              // throw a miss
-          } else if(Grid.grid[i][j] == -1){
-            // throw a repeat hit
-          } else {
-            // throw a hit
+          if(Grid.grid[shotArray[i]][shotArray[j]] == 0){
+              //This is the value for misses, they will be displayed on the baord for one turn
+              Grid.grid[shotArray[i]][shotArray[j]] = -3;
+              scoreCard.misses = scoreCard.misses + 1;
+              scoreCard.remaining = scoreCard.remaining - 1;
+          } else if(Grid.grid[shotArray[i]][shotArray[j]] == 1){
+              //This is the value for hits, they will be displayed on the board for one turn
+              Grid.grid[shotArray[i]][shotArray[j]] = -2;
+              scoreCard.hits = scorecard.hits + 1;
+              scoreCard.remaining = scoreCard.remaining - 1;
+          } else if(Grid.grid[shotArray[i]][shotArray[j]] == -2 || Grid.grid[shotArray[i]][shotArray[j]] == -3){
+              //This is the value that will show hits that have been made more than one turn ago,
+              //Hits and misses will be dislayed on the board for one turn
+              Grid.grid[shotArray[i]][shotArray[j]] = -1;
           }
         }
       }
-      return [Grid,tally];
+      return [Grid,scoreCard];
 }
 
 
--- src/main/scripts/website_script.js
+++ src/main/scripts/website_script.js
@@ -1,4 +1,4 @@
-var createGrid = require('./BattleField/grid').Grid;
+var Grid = require('./BattleField/grid').Grid;
 var createTargets = require('./AttackScripts/CalcShots').createTargets;
 var calcShot = require('./AttackScripts/CalcShots').calcShot;
 var userSelection = require('./UserCommands/Selection').userSelection;
@@ -8,6 +8,66 @@ var tableView = document.getElementById('battleGrid');
 var consoleView = document.getElementById('intelTerminal');
 
 /*
-* The functions in this script will act as the controller for the model classes
+* The functions in this script will act as the controlller/model for mechanical and creation scripts
 * that are imported above. The Table and ConsoleViews will actas event handlers
 */
+
+
+//These variables will act as a stored model of the page
+var battleField = new Grid();
+var scoreCard = {
+  hits: 0,
+  misses: 0,
+  remaining: 100;
+}
+
+renderGrid();
+
+function gridClickHandler(x, y) {
+//Create the necessary functions to calculate the shot
+  var selection = new userSelection(x, y);
+  var targets = createTargets(Grid, selection);
+  var doneShot = calcShot(targets, selection, Grid, scoreCard);
+
+//Store shot stats in temporary variables
+  score = doneShot[1];
+  battleField = doneShot[0];
+
+//Push the temporary variables into render arrays
+  renderScore(score);
+  renderGrid(battleField);
+
+//Replace stored models of the page with
+  this.score = score;
+  this.battleField = battleField;
+
+}
+
+function renderGrid(battleField) {
+    for(var i = 0; i < battleField.length; i++){
+      for(var j = 0; j < battleField[i].length; j++){
+        if(battleField[i][j] == -1){
+            //change tr(i by id) and td(j by class) - disappear
+        }
+        else if(battleField[i][j] == -2){
+          // make the border invisible and put the word "hit"
+        }
+        else if(battleField[i][j] == -3) {
+          // put miss for one turn
+        }
+        else {
+          // keep the tile the same
+        }
+      }
+    }
+    return battleField;
+}
+
+function renderScore(score) {
+    //display hits
+
+    //display misses
+
+    //display remaining tiles
+
+}
